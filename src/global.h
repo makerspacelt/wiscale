6d3c4a0d163b732bcbe850bb3d4d4e61b22c6295
Linas Minkevičius <minkevicius.linas@gmail.com>
2022-10-20 19:38:25 +0300

    Added configs based on mqtt json

--- src/global.h
+++ src/global.h
@@ -6,18 +6,60 @@
 #define WIFI_PASS "PASS"
 
 #define EXECUTION_TIMEOUT 10
+#define MAX_GPIO_PINS 24
+#define MAX_ADC_PINS 2
 
-struct globalConfig {
-    int battery_range = 4860;
-    float piece_grams = 85.0;
-    float scale_zero = 185000;
-    float scale_cal = 402.80;
-    int scale_gain = 32;
-    int led_pin = 2;
-    const char* name = (char *)"ESP-D4EFA3";
+struct GPIOConfig
+{
+    uint8_t pin = 2;
+    const char *name = (char *)"NA";
+    uint8_t mode = 0; // 0 - input, 1 - output
+    uint8_t inverted = 0;
+    uint8_t defaultValue = 0;
+    uint8_t configured = 0;
 };
 
-struct globalState {
+struct ADCConfig
+{
+    uint8_t pin = 17; // A0
+    const char *name = (char *)"NA";
+    uint32_t readingsForMean = 10;
+    float offset = 0;
+    float multiplier = 0.0047460937; // How to convert to voltage.
+    uint8_t configured = 0;
+};
+
+struct Hx711Config
+{
+    const char *name = (char *)"weight";
+    uint8_t pin_sck = 5;
+    uint8_t pin_dt = 4;
+    uint8_t gain = 32;
+    int offset = 185000;
+    float multi = 402.8;
+};
+
+struct Ds18b20Config
+{
+    const char *name = (char *)"temp";
+    uint8_t pin = 1;
+    float offset = 0;
+    float multi = 1;
+};
+
+struct globalConfig
+{
+    int battery_range = 4860;
+    float piece_grams = 85.0;
+    const char *name = (char *)"ESP-D4EFA3";
+    GPIOConfig gpio[MAX_GPIO_PINS] = {}; // structs must be initialized with {} to prevent garbage data
+    ADCConfig adc[MAX_ADC_PINS] = {};
+    Hx711Config scale = {};
+    Ds18b20Config temperature = {};
+};
+
+struct globalState
+{
     float grams = 0;
     float battery = 0;
     float temperature = 0;
@@ -28,6 +70,5 @@ struct globalState {
 
 extern globalState state;
 extern globalConfig config;
-
 
 #endif // GLOBAL_H
