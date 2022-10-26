32049287f9abaea2aa1be272e0384c0903ac4435
Linas <minkevicius.linas@gmail.com>
2022-10-26 19:47:11 +0300

    initial prototypes

--- src/temperature.cpp
+++ src/temperature.cpp
@@ -9,6 +9,17 @@ void readTemperature()
 	sensors.requestTemperatures(); // Send the command to get temperatures
 	State.temperature = sensors.getTempCByIndex(0);
 }
+
+
+Ds18b20Config readTemperature(uint16_t sensorId){
+	float temperature== sensors.getTempCByIndex(0);
+	Ds18b20Config readings={};
+
+}
+Ds18b20Config[] readAllTemperatureSensors();
+
+
+
 void ReconfigureTemperature()
 {
 	// Start up tempreture
--- src/temperature.h
+++ src/temperature.h
@@ -5,9 +5,16 @@
 #include <DallasTemperature.h>
 #include <OneWire.h>
 
+
 // TODO get this from config
 #define ONE_WIRE_BUS 14 //D5
+#define NUMBER_OF_TEMPERATURE_SENSORS 1
 
+// Reads first temperature sensor
 void readTemperature();
+
+Ds18b20Config readTemperature(uint16_t sensorId);
+Ds18b20Config[] readAllTemperatureSensors();
+
 void ReconfigureTemperature();
 #endif
